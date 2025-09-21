88c87e919350042d384bdc4def535444ab9e76ae
Marlon Ramirez Cruz <161239307+slashmarlon@users.noreply.github.com>
2025-09-20 18:16:51 -0600

    Update preliminar_sensor.h

--- Codigos/preliminar_sensor.h
+++ Codigos/preliminar_sensor.h
@@ -1,1 +1,16 @@
+#ifndef SS_UTILS_H_
+#define SS_UTILS_H_
 
+#include <Arduino.h> 
+
+namespace SS_Utils {
+
+    // Inicializa el pin del sensor.
+    void setup(uint8_t analogPin);
+
+    // Lee el nivel de agua y lo asigna a un string.
+    String readWaterLevel();
+
+}
+
+#endif
