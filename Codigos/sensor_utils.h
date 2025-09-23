c6b25e6caeee4b15846884fd6ef75e8036633549
Marlon Ramirez Cruz <161239307+slashmarlon@users.noreply.github.com>
2025-09-22 21:43:24 -0600

    Update sensor_utils.h

--- Codigos/sensor_utils.h
+++ Codigos/sensor_utils.h
@@ -4,14 +4,14 @@
 #include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>   // Para sensor de temperatura DS18B20.
-// Para el sensor de pH y nivel de agua se usa lectura analógica.
+// Para el sensor de turbidez y nivel de agua se usa lectura analógica.
 
 namespace SENSORS_Utils {
 
     // Pines de sensores.
     const int TEMP_PIN = 2;       // Pin de datos del DS18B20.
     const int WATER_PIN = A0;     // Entrada analógica nivel de agua.
-    const int PH_PIN = A1;        // Entrada analógica sensor de pH.
+    const int TURB_PIN = A1;        // Entrada analógica sensor de turbidez.
 
     // Inicialización del bus OneWire para el sensor de temperatura.
     extern OneWire oneWire;
@@ -21,9 +21,9 @@ namespace SENSORS_Utils {
     void initSensors();   // Inicializa sensores.
     String readTemperature(); // Devuelve temperatura en °C como String.
     String readWaterLevel();  // Devuelve nivel de agua como String.
-    String readPH();          // Devuelve valor de pH como String.
+    String readTurbidity();   // Devuelve valor de turbidez como String.
     String getAllReadings(); // Une todas las lecturas en un solo String.
 
-}
+    }
 
 #endif
