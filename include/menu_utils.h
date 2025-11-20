7a50d97d182bf5599822a2bf2ba1bb413ecb3f9b
Marlon Ramirez Cruz <161239307+slashmarlon@users.noreply.github.com>
2025-11-19 18:05:46 -0600

    Create menu_utils.h

---
+++ include/menu_utils.h
@@ -0,0 +1,19 @@
+/*
+
+*/
+
+#ifndef MENU_UTILS_H_
+#define MENU_UTILS_H_
+
+#include <Arduino.h>
+
+namespace MENU_Utils {
+
+    const String checkBTType();
+    const String checkProcessActive(const bool process);
+    const String screenBrightnessAsString(const uint8_t bright);
+    void  showOnScreen();
+
+}
+
+#endif
